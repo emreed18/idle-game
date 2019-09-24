422533f8a8c17776f0081ba541e3acc30f25e49d
elijah reed <emreed18@harrisonburg.k12.va.us>
2019-09-24 14:30:18 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,11 +1,12 @@
 var score = 1; // Create a variable to store the score in
 
-var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('click to go faster', btnPress);   // Create a button element
-var hidden = new Button('Boo!');
+var title = new Title('Welcome to the jungle'); // Create a title element
+var button = new Button('coins', btnPress);   // Create a button element
+var hidden = new Button('coins');
+var pl1 = new Button('player 1');
 hidden.hide();
 var scr = new Text(score);                    // Create a text element
-
+var pl1 = new Button('player 2')
 setInterval(btnPress, 1);   // Set up a loop
 
 function btnPress() {
